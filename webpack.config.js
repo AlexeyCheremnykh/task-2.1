b423452d1d3e68d49152848391913bf2a8cf42ef
AlexeyCheremnykh <chmnkh@gmail.com>
2017-11-19 16:54:52 +0700

    Нерезиновый готов

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'), 
         filename: 'bundle.js',
-    },
-    watch: true,
+    },      
     module: {
         rules: [
             { 
