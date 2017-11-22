d208351841f807cf5dcf4feb130787dacf92af3a
AlexeyCheremnykh <chmnkh@gmail.com>
2017-11-22 22:43:05 +0700

    index.styl для шрифтов сделан

--- entry.js
+++ entry.js
@@ -1,5 +1,6 @@
 import './src/fonts/fonts.css';
-import './src/blocks/main-container/main-container.styl'
+import './src/blocks/index.styl';
+import './src/blocks/main-container/main-container.styl';
 import './src/blocks/first-container/first-container.styl';
 import './src/blocks/main-info/main-info.styl';
 import './src/blocks/block-header/block-header.styl';
@@ -7,7 +8,7 @@ import './src/blocks/profile/profile.styl';
 import './src/blocks/contact/contact.styl';
 import './src/blocks/skills/skills.styl';
 import './src/blocks/second-container/second-container.styl';
-import './src/blocks/education/education.styl'
-import './src/blocks/common-list/common-list.styl'
-import './src/blocks/experience/experience.styl'
-import './src/blocks/software/software.styl'
+import './src/blocks/education/education.styl';
+import './src/blocks/common-list/common-list.styl';
+import './src/blocks/experience/experience.styl';
+import './src/blocks/software/software.styl';
