b4a6615b7ae5f8dcc7746d07337e7e8507ce3301
AlexeyCheremnykh <chmnkh@gmail.com>
2018-01-29 15:54:35 +0700

    переименовал колонки и сделал через флоаты

--- entry.js
+++ entry.js
@@ -1,12 +1,12 @@
 import './src/fonts/fonts.css';
 import './src/blocks/index.styl';
-import './src/blocks/first-container/first-container.styl';
+import './src/blocks/cv-column-first/cv-column-first.styl';
 import './src/blocks/main-info/main-info.styl';
 import './src/blocks/block-header/block-header.styl';
 import './src/blocks/profile/profile.styl';
 import './src/blocks/contact/contact.styl';
 import './src/blocks/skills/skills.styl';
-import './src/blocks/second-container/second-container.styl';
+import './src/blocks/cv-column-second/cv-column-second.styl';
 import './src/blocks/education/education.styl';
 import './src/blocks/common-list/common-list.styl';
 import './src/blocks/experience/experience.styl';
