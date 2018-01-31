f91f3d18b134974da0bd28037f199da05ea3706c
AlexeyCheremnykh <chmnkh@gmail.com>
2018-01-31 16:28:41 +0700

    Все доделал и пересобрал

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,5 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-/*var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-var extractPlugin = new ExtractTextPlugin({
-    filename: 'styles.css'
-});*/
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './entry.js',
@@ -16,20 +11,18 @@ module.exports = {
         rules: [
             {
                 test: /\.styl$/,
-                //use: extractPlugin.extract({
                 use: [
                     'style-loader',
                     'css-loader',
-                    'stylus-loader'
-                ]
-                //})
+                    'stylus-loader',
+                ],
             },
             {
                 test: /\.css$/,
                 use: [
                     'style-loader',
-                    'css-loader'
-                ]
+                    'css-loader',
+                ],
             },
             {
                 test: /\.png$/,
@@ -37,32 +30,34 @@ module.exports = {
                     {
                         loader: 'file-loader',
                         options: {
-                            name: 'img/[name].[ext]'
-                        }
-                    }
-                ]
+                            name: 'img/[name].[ext]',
+                        },
+                    },
+                ],
             },
             {
-                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                test: /\.(svg|ttf|woff)$/,
                 use: [
                     {
                         loader: 'file-loader',
-                        options : {
-                            name: 'fonts/[name].[ext]'
-                        }
-                    }
-                ]
+                        options: {
+                            name: 'fonts/[name].[ext]',
+                        },
+                    },
+                ],
             },
             {
                 test: /\.pug$/,
-                use: ['html-loader', 'pug-html-loader']
-            }
-        ]
+                use: [
+                    'html-loader',
+                    'pug-html-loader',
+                ],
+            },
+        ],
     },
     plugins: [
-        //extractPlugin,
         new HtmlWebpackPlugin({
-            template: 'src/pages/index.pug'
-        })
-    ]
+            template: 'src/pages/index.pug',
+        }),
+    ],
 };
