e9e7fa0a5edd5a55f92fe156b2233b488b0b26d6
AlexeyCheremnykh <chmnkh@gmail.com>
2018-01-31 16:55:04 +0700

    entry.js положил в src

---
+++ src/entry.js
@@ -0,0 +1,13 @@
+import './fonts/PT Sans/pt-sans.css';
+import './pages/index.styl';
+import './blocks/cv-column-first/cv-column-first.styl';
+import './blocks/applicant/applicant.styl';
+import './blocks/cv-section-header/cv-section-header.styl';
+import './blocks/profile/profile.styl';
+import './blocks/contact/contact.styl';
+import './blocks/skills/skills.styl';
+import './blocks/cv-column-second/cv-column-second.styl';
+import './blocks/education/education.styl';
+import './blocks/cv-list/cv-list.styl';
+import './blocks/experience/experience.styl';
+import './blocks/software/software.styl';
