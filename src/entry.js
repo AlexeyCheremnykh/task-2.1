a905dc023ab7baa036f4b28954c8c79cdab71305
AlexeyCheremnykh <chmnkh@gmail.com>
2018-02-08 12:09:29 +0700

    добавил блокам префикс cv-

--- src/entry.js
+++ src/entry.js
@@ -1,13 +1,14 @@
 import './fonts/PT Sans/pt-sans.css';
 import './pages/index.styl';
-import './blocks/cv-column-first/cv-column-first.styl';
-import './blocks/applicant/applicant.styl';
+
 import './blocks/cv-section-header/cv-section-header.styl';
-import './blocks/profile/profile.styl';
-import './blocks/contact/contact.styl';
-import './blocks/skills/skills.styl';
-import './blocks/cv-column-second/cv-column-second.styl';
-import './blocks/education/education.styl';
+import './blocks/cv-applicant/cv-applicant.styl';
+import './blocks/cv-profile/cv-profile.styl';
+import './blocks/cv-contact/cv-contact.styl';
+import './blocks/cv-skills/cv-skills.styl';
+import './blocks/cv-column-first/cv-column-first.styl';
 import './blocks/cv-list/cv-list.styl';
-import './blocks/experience/experience.styl';
-import './blocks/software/software.styl';
+import './blocks/cv-education/cv-education.styl';
+import './blocks/cv-experience/cv-experience.styl';
+import './blocks/cv-software/cv-software.styl';
+import './blocks/cv-column-second/cv-column-second.styl';
