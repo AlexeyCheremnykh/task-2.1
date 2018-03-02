0f5dbefbb61aa00cd4cee19a41c61db43b31954c
AlexeyCheremnykh <chmnkh@gmail.com>
2018-03-02 17:45:32 +0700

    #11 убрал у list и section-header

--- src/entry.js
+++ src/entry.js
@@ -2,14 +2,14 @@ import './fonts/PT Sans/pt-sans.css';
 import './favicons/favicons';
 import './pages/index.styl';
 
-import './blocks/cv-section-header/cv-section-header.styl';
+import './blocks/section-header/section-header.styl';
 import './blocks/applicant/applicant.styl';
 import './blocks/profile/profile.styl';
 import './blocks/contact/contact.styl';
 import './blocks/skills/skills.styl';
 import './blocks/column-left/column-left.styl';
 import './blocks/applicant-competence/applicant-competence.styl';
-import './blocks/cv-list/cv-list.styl';
+import './blocks/list/list.styl';
 import './blocks/education/education.styl';
 import './blocks/experience/experience.styl';
 import './blocks/software/software.styl';
